5a08bf036de98e23547b6d06e5edfde5a375447f
DylanWTR <dylan.winter@epitech.eu>
2024-11-30 17:48:23 -0500

    values

--- config/ranks.py
+++ config/ranks.py
@@ -1,21 +1,43 @@
+# RANGES_VALUES = {
+#     "1-50": 68,
+#     "51-100": 363,
+#     "101-150": 1082,
+#     "151-200": 2830,
+#     "200+": 6177,
+# }
+
+# RANKS_TRESHOLDS = {
+#     "Unranked": 0,
+#     "Iron": 68,
+#     "Bronze": 136,
+#     "Silver": 363,
+#     "Gold": 726,
+#     "Platinum": 1082,
+#     "Emerald": 2164,
+#     "Diamond": 2830,
+#     "Master": 5660,
+#     "GrandMaster": 6177,
+#     "Challenger": 12354,
+# }
+
 RANGES_VALUES = {
     "1-50": 68,
-    "51-100": 363,
-    "101-150": 1082,
-    "151-200": 2830,
-    "200+": 6177,
+    "51-100": 227,
+    "101-150": 356,
+    "151-200": 666,
+    "200+": 517,
 }
 
 RANKS_TRESHOLDS = {
     "Unranked": 0,
     "Iron": 68,
     "Bronze": 136,
-    "Silver": 363,
-    "Gold": 726,
-    "Platinum": 1082,
-    "Emerald": 2164,
-    "Diamond": 2830,
-    "Master": 5660,
-    "GrandMaster": 6177,
-    "Challenger": 12354,
+    "Silver": 227,
+    "Gold": 454,
+    "Platinum": 356,
+    "Emerald": 712,
+    "Diamond": 666,
+    "Master": 1332,
+    "GrandMaster": 517,
+    "Challenger": 1034
 }
